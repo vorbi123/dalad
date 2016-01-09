e1f578c4134d96faea537056c86f8a736cda2c54
DALADteam <niusha222222@gmail.com>
2016-01-09 14:07:45 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -222,7 +222,7 @@ function create_config( )
     },
     sudo_users = {156513822},--Sudo users
     disabled_channels = {},
-    realm = {},--Realms Id
+    realm = {91313662},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
