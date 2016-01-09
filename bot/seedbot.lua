b2a463cbeb93e9a231724eaf175b0507a27746ff
DALADteam <niusha222222@gmail.com>
2016-01-09 17:20:47 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {156513822},--Sudo users
+    sudo_users = {156513822,165397171},--Sudo users
     disabled_channels = {},
     realm = {91313662},--Realms Id
     moderation = {data = 'data/moderation.json'},
